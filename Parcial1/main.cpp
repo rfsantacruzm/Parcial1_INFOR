10c21866b1f3de7adfa9cb8c09be038a0ecb1e90
rfsantacruzm <francisco.santacruz@udea.edu.co>
2024-03-23 18:30:21 -0500

    segundo commit

--- Parcial1/main.cpp
+++ Parcial1/main.cpp
@@ -28,11 +28,26 @@ int main()
     cout << "Ingrese el tamano de la clave k: " <<endl;
     cin>> k;
     int* clave = new int;
-    for(int i = 0; i < k; ++i){
-        cout <<"Ingrese el digito en la pos "<< i+1<< ": ";
-        cin>>n;
-        clave[i] = n;
+    bool kvalid = false;
+    while (kvalid == false) {
+        for(int i = 0; i < k; ++i){
+            cout <<"Ingrese el digito en la pos "<< i+1<< ": ";
+            cin>>n;
+            clave[i] = n;
+        }
+        for(int j=2; j<k; ++j){
+            if(clave[j]< -1 || clave[j]>1){
+                cout<<"Clave no valida, intente nuevamente"<<endl;
+                kvalid = false;
+                break;
+            }
+            else{
+                kvalid = true;
+            }
+        }
     }
     int tamano = definirtamano(clave);
+    int cantm = k-1;
+
     return 0;
 }
